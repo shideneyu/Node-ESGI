ef380040d92c1f11769b1f8bdca8c57f7e490142
zak000 <zak.essadeq@gmail.Com>
2016-03-25 11:42:11 +0100

    delete user

--- actions/users/remove.js
+++ actions/users/remove.js
@@ -1,1 +1,16 @@
-remove
+module.exports = function(server)
+{
+	return function(req, res, next)
+	{
+
+		var User = server.models.User;
+		User.remove({_id: req.params.id}, function(err, data)
+		{
+			if(err)
+			return res.status(500).send(err);
+			res.send(data);
+		});
+
+	}
+
+}
