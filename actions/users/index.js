3e68bbc712c210fafafe1bd69e1ad34a25f8b6b7
Alex-60 <alex.legros60@gmail.com>
2016-03-25 11:41:34 +0100

    Create User

--- actions/users/index.js
+++ actions/users/index.js
@@ -1,1 +1,9 @@
-index
+module.exports = function(server){
+  return {
+    create: require('./create')(server),
+    get: require('./get')(server),
+    show: require('./show')(server),
+    delete: require('./delete')(server),
+    update: require('./update')(server)
+  }
+};
